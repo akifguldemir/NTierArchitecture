2718b5534c51fd15cf4e613d9b574002796944b4
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-01 14:05:42 +0300

    Job class and related classes added

---
+++ DataAccessLayer/Migrations/20240801103215_mig_addjob.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_addjob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobID",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_JobID",
+                table: "Customer",
+                column: "JobID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customer_Jobs_JobID",
+                table: "Customer",
+                column: "JobID",
+                principalTable: "Jobs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customer_Jobs_JobID",
+                table: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customer_JobID",
+                table: "Customer");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "JobID",
+                table: "Customer");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "CategoryId");
+        }
+    }
+}
