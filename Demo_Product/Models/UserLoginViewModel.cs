49cb1ffd5e2bcc7cfb7cb974dcb2f3c7610d203c
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-03 14:24:04 +0300

    Login process completed

---
+++ Demo_Product/Models/UserLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı gir")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Şifre gir")]
+        public string Password { get; set; }
+    }
+}
